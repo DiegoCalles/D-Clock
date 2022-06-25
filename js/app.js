d5ca0ce0c50cf45b5b7e7f89a236ffb1c0f2e63d
DiegoCalles WebDev <diegocalles99@gmail.com>
2022-06-25 14:54:58 -0300

    relojDigital app added

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const mostrarReloj = ()=>{
+    console.log('funcion mostrarReloj');
+    
+    let fecha = new Date();
+    let hora = FormatoHora(fecha.getHours());
+    let minuto = FormatoHora( fecha.getMinutes());
+    let segundo = FormatoHora(fecha.getSeconds());
+    let horaCompleta = `${hora}: ${minuto}: ${segundo}`;
+    document.getElementById('hora').innerHTML = horaCompleta;
+}
+// para que le agregue un cero (0) cuando sea menor a diez
+const FormatoHora=(hora)=>{
+    if(hora<10)
+    hora = '0' +hora;
+    return hora;
+}
+setInterval(mostrarReloj,1000);
+
+const mostrarFecha= ()=>{
+    console.log('funcion mostrarFecha')
+    let fecha = new Date();
+    const weekday = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+    let diaString = weekday[fecha.getDay()];
+    let diaNumber = FormatoHora(fecha.getDate());// para que el dia en numero tambien empiece con doble digito
+    const MonthsString =['Jan','Feb','Mar','April','May','June','July','Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+      
+    let mesLetra = MonthsString[fecha.getMonth()];
+
+    let fechaCompleta = `${diaString}, ${diaNumber} ${mesLetra}`;
+    document.getElementById('fecha').innerHTML = fechaCompleta;
+}
