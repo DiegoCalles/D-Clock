f118384096ce6671636bf7f51953ea7c415de6d7
DiegoCalles WebDev <diegocalles99@gmail.com>
2022-06-27 06:22:36 -0300

    app.js and styles.css new commits

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ const mostrarReloj = ()=>{
     let segundo = FormatoHora(fecha.getSeconds());
     let horaCompleta = `${hora}: ${minuto}: ${segundo}`;
     document.getElementById('hora').innerHTML = horaCompleta;
+
+    // classlist = para recuperar todas las clases que se estan uilizando en css a este elemento html
+    // toggle= funciona como un interruptor. apaga y prende cada vez que se aplica el set interval que manda a llamar a mostrarReloj()
+    // t se aplica a la clase animar de css
+    document.getElementById('contenedor').classList.toggle('animar');
 }
 // para que le agregue un cero (0) cuando sea menor a diez
 const FormatoHora=(hora)=>{
@@ -19,6 +24,7 @@ setInterval(mostrarReloj,1000);
 const mostrarFecha= ()=>{
     console.log('funcion mostrarFecha')
     let fecha = new Date();
+// para mostrar los meses y dias creo un array con los strings y dentro del mismo aplico los metodos del objeto Date (.getDay, getMonth)
     const weekday = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
     let diaString = weekday[fecha.getDay()];
     let diaNumber = FormatoHora(fecha.getDate());// para que el dia en numero tambien empiece con doble digito
